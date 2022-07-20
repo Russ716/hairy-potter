95c8edfb99aee100d2ee82dd7fd67454563bcb2e
Russ Pelton <iamkrashenbern@yahoo.com>
2022-07-20 11:46:32 -0500

    Made pottery at the Wheel

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,9 @@
+export const makePottery = (clayShape, clayWeight, clayHeight) => {
+    let clayObject = {
+        shape: clayShape,
+        weight: clayWeight,
+        height: clayHeight,
+        id: 1
+    }
+    return clayObject
+}
---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+// Imports go first
+import {makePottery} from "./PotteryWheel.js"
+
+
+// Make 5 pieces of pottery at the wheel
+let mug = makePottery("Cocoa Mug", 5, 4)
+let ashtray = makePottery("Cigar rest", 3, 1)
+let chipDip = makePottery("Chip 'n Dip", 12, 6)
+let centerpiece = makePottery("Cornucopia", 25, 15)
+let vase = makePottery("Carnations vase", 10, 15)
+
+// Fire each piece of pottery in the kiln
+
+
+// Determine which ones should be sold, and their price
+
+
+// Invoke the component function that renders the HTML list
+
+
+
